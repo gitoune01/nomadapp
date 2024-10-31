8b41a51e30e08dd103afe0048b4483b5597ad170
gitoune01 <docgyno@gmail.com>
2024-10-31 18:13:59 +0100

    first commit

---
+++ src/components/Title/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { styles } from './styles';
+
+const Title = () => {
+  return <Text style={styles.title}>Title</Text>;
+};
+
+export default Title;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Title from '../../components/Title'
+
+const Home = () => {
+  return (
+    <View>
+       <Title />
+    </View>
+  )
+}
+
+export default Home
